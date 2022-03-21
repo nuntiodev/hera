2e1c02e509c9958cfe820a662a885d101a9612e7
Oscar A. B. Orellana <oscar@softcorp.io>
2022-03-21 15:03:55 +0100

    updated

--- repository/user_repository/helper.go
+++ repository/user_repository/helper.go
@@ -99,7 +99,6 @@ func validatePassword(password string) error {
 	passwordValidator := validator.New(
 		validator.MinLength(10, errors.New("password needs to contain at least 5 chars")),
 		validator.MaxLength(100, errors.New("password needs to contain at below 100 chars")),
-		validator.ContainsAtLeast("0123456789", 1, errors.New("password needs to contain at least one number")),
 	)
 	if err := passwordValidator.Validate(password); err != nil {
 		return err

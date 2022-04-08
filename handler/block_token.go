75d72ef556073403e5320d5035d937f3a48486bd
Oscar A. B. Orellana <oscar@softcorp.io>
2022-04-08 18:41:26 +0200

    updated

--- handler/block_token.go
+++ handler/block_token.go
@@ -2,20 +2,11 @@ package handler
 
 import (
 	"context"
-	"errors"
 	"github.com/softcorp-io/block-proto/go_block"
 )
 
 func (h *defaultHandler) BlockToken(ctx context.Context, req *go_block.UserRequest) (*go_block.UserResponse, error) {
-	token := ""
-	if req.Token.AccessToken != "" {
-		token = req.Token.AccessToken //todo: create new field just named token
-	} else if req.Token.RefreshToken != "" {
-		token = req.Token.RefreshToken
-	} else {
-		return &go_block.UserResponse{}, errors.New("no token in request")
-	}
-	customClaims, err := h.token.ValidateToken(publicKey, token)
+	customClaims, err := h.token.ValidateToken(publicKey, req.Token.TokenPointer)
 	if err != nil {
 		return &go_block.UserResponse{}, err
 	}

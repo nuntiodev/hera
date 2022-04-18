e6e452d1a8d22e64666785ff56c2629d31ac3207
Oscar A. B. Orellana <oscar@softcorp.io>
2022-04-18 14:08:46 +0200

    updateD

--- initializer/initializer.go
+++ initializer/initializer.go
@@ -17,15 +17,13 @@ import (
 )
 
 const (
-	BLOCK_USER_RSA_SECRET_NAME = "user-block-rsa-secret"
-	BLOCK_USER_ENC_SECRET_NAME = "user-block-enc–secret"
-	EngineKubernetes           = "kubernetes"
-	EngineMemory               = "memory"
+	blockUserSecretName = "user-block-secret"
+	EngineKubernetes    = "kubernetes"
+	EngineMemory        = "memory"
 )
 
 type Initializer interface {
-	CreateRSASecrets(ctx context.Context) error
-	CreateEncryptionSecret(ctx context.Context) error
+	CreateSecrets(ctx context.Context) error
 }
 
 type defaultInitializer struct {
@@ -69,14 +67,14 @@ func New(zapLog *zap.Logger, engine string) (*defaultInitializer, error) {
 	}, nil
 }
 
-func (i *defaultInitializer) CreateRsaSecrets(ctx context.Context) error {
-	if os.Getenv("PUBLIC_KEY") != "" || os.Getenv("PRIVATE_KEY") != "" {
-		i.zapLog.Info("RSA keys is already provided internally by the system (the PUBLIC_KEY or PRIVATE_KEY variable is set).")
+func (i *defaultInitializer) CreateSecrets(ctx context.Context) error {
+	if os.Getenv("PUBLIC_KEY") != "" && os.Getenv("PRIVATE_KEY") != "" && os.Getenv("ENCRYPTION_KEYS") != "" {
+		i.zapLog.Info("Secrets is already provided internally by the system (the PUBLIC_KEY, ENCRYPTION_KEYS and PRIVATE_KEY variable is set).")
 		return nil
 	}
 	// check if secret already exists
-	if cryptoSecret, err := i.k8s.CoreV1().Secrets(i.namespace).Get(ctx, BLOCK_USER_RSA_SECRET_NAME, metav1.GetOptions{}); err != nil {
-		i.zapLog.Info("Block user RSA secret does not exist... creating....")
+	if cryptoSecret, err := i.k8s.CoreV1().Secrets(i.namespace).Get(ctx, blockUserSecretName, metav1.GetOptions{}); err != nil {
+		i.zapLog.Info("Block user secret does not exist... creating....")
 		// create public private keys
 		rsaPrivateKey, rsaPublicKey, err := i.crypto.GenerateRsaKeyPair(2048)
 		if err != nil {
@@ -90,15 +88,21 @@ func (i *defaultInitializer) CreateRsaSecrets(ctx context.Context) error {
 			Type:  "RSA PUBLIC KEY",
 			Bytes: x509.MarshalPKCS1PublicKey(rsaPublicKey),
 		}))
+		// create encryption secret
+		encryptionSecret, err := i.crypto.GenerateSymmetricKey(32, cryptox.AlphaNum)
+		if err != nil {
+			return err
+		}
 		// create secret in the EngineKubernetes api
 		secretData := map[string]string{
-			"PRIVATE_KEY": userPrivateKey,
-			"PUBLIC_KEY":  userPublicKey,
+			"PRIVATE_KEY":     userPrivateKey,
+			"PUBLIC_KEY":      userPublicKey,
+			"ENCRYPTION_KEYS": encryptionSecret,
 		}
 		secret := &v1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
 				Namespace: i.namespace,
-				Name:      BLOCK_USER_RSA_SECRET_NAME,
+				Name:      blockUserSecretName,
 			},
 			StringData: secretData,
 			Type:       v1.SecretTypeOpaque,
@@ -113,54 +117,11 @@ func (i *defaultInitializer) CreateRsaSecrets(ctx context.Context) error {
 		if err := os.Setenv("PRIVATE_KEY", userPrivateKey); err != nil {
 			return err
 		}
-		i.zapLog.Info("Successfully created RSA secret")
-	} else {
-		if err := os.Setenv("PUBLIC_KEY", string(cryptoSecret.Data["PUBLIC_KEY"])); err != nil {
-			return err
-		}
-		if err := os.Setenv("PRIVATE_KEY", string(cryptoSecret.Data["PRIVATE_KEY"])); err != nil {
-			return err
-		}
-		i.zapLog.Info("RSA secret already exists")
-	}
-	return nil
-}
-
-func (i *defaultInitializer) CreateEncryptionSecret(ctx context.Context) error {
-	if os.Getenv("ENCRYPTION_KEYS") != "" {
-		i.zapLog.Info("Encryption keys is already provided internally by the system (the ENCRYPTION_KEYS variable is set).")
-		return nil
-	}
-	// check if secret already exists
-	if cryptoSecret, err := i.k8s.CoreV1().Secrets(i.namespace).Get(ctx, BLOCK_USER_ENC_SECRET_NAME, metav1.GetOptions{}); err != nil {
-		i.zapLog.Info("Block user encryption secret does not exist... creating....")
-		// create encryption secret
-		encryptionSecret, err := i.crypto.GenerateSymmetricKey(32, cryptox.AlphaNum)
-		if err != nil {
-			return err
-		}
-		// create secret in the EngineKubernetes api
-		secretData := map[string]string{
-			"ENCRYPTION_KEYS": encryptionSecret,
-		}
-		secret := &v1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: i.namespace,
-				Name:      BLOCK_USER_ENC_SECRET_NAME,
-			},
-			StringData: secretData,
-			Type:       v1.SecretTypeOpaque,
-		}
-		if _, err := i.k8s.CoreV1().Secrets(i.namespace).Create(ctx, secret, metav1.CreateOptions{}); err != nil {
-			return err
-		}
-		// set in memory
 		if err := os.Setenv("ENCRYPTION_KEYS", encryptionSecret); err != nil {
 			return err
 		}
-		i.zapLog.Info("Successfully created encryption secret")
+		i.zapLog.Info("Successfully created secret")
 	} else {
-		i.zapLog.Info("Encryption secret already exists")
 		newEncryptionKey := strings.TrimSpace(os.Getenv("NEW_ENCRYPTION_KEY"))
 		newKeyAlreadyExists := false
 		encryptionKeys := strings.Fields(string(cryptoSecret.Data["ENCRYPTION_KEYS"]))
@@ -174,11 +135,13 @@ func (i *defaultInitializer) CreateEncryptionSecret(ctx context.Context) error {
 			encryptionKeys = append(encryptionKeys, newEncryptionKey)
 			secretData := map[string]string{
 				"ENCRYPTION_KEYS": strings.Join(encryptionKeys, " "),
+				"PRIVATE_KEY":     string(cryptoSecret.Data["PUBLIC_KEY"]),
+				"PUBLIC_KEY":      string(cryptoSecret.Data["PUBLIC_KEY"]),
 			}
 			if _, err := i.k8s.CoreV1().Secrets(i.namespace).Update(ctx, &v1.Secret{
 				ObjectMeta: metav1.ObjectMeta{
 					Namespace: i.namespace,
-					Name:      BLOCK_USER_ENC_SECRET_NAME,
+					Name:      blockUserSecretName,
 				},
 				StringData: secretData,
 				Type:       v1.SecretTypeOpaque,
@@ -187,9 +150,16 @@ func (i *defaultInitializer) CreateEncryptionSecret(ctx context.Context) error {
 			}
 		}
 		// set in memory
+		if err := os.Setenv("PUBLIC_KEY", string(cryptoSecret.Data["PUBLIC_KEY"])); err != nil {
+			return err
+		}
+		if err := os.Setenv("PRIVATE_KEY", string(cryptoSecret.Data["PRIVATE_KEY"])); err != nil {
+			return err
+		}
 		if err := os.Setenv("ENCRYPTION_KEYS", strings.Join(encryptionKeys, " ")); err != nil {
 			return err
 		}
+		i.zapLog.Info("RSA secret already exists")
 	}
 	return nil
 }

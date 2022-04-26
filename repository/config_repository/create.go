0ca3fe0d7bfb64defd27357c9d03178cebbb4cfc
Oscar A. B. Orellana <oscar@softcorp.io>
2022-04-26 10:46:02 +0200

    updated

--- repository/config_repository/create.go
+++ repository/config_repository/create.go
@@ -12,6 +12,27 @@ func (cr *defaultConfigRepository) Create(ctx context.Context, config *go_block.
 		return nil, errors.New("missing required config")
 	}
 	create := ProtoConfigToConfig(config)
+	create.EnableNuntioConnect = true
+	create.DisableDefaultSignup = false
+	create.DisableDefaultLogin = false
+	create.ValidatePassword = true
+	create.AuthConfig = &AuthConfig{
+		WelcomeTitle:              "Welcome",
+		WelcomeDetails:            "Welcome Details",
+		LoginButton:               "Login",
+		LoginTitle:                "Login",
+		LoginDetails:              "Fill in the details below to login to your account",
+		RegisterButton:            "Register",
+		RegisterTitle:             "Register",
+		RegisterDetails:           "Fill in the details below to register for an account",
+		MissingPasswordTitle:      "Missing required password",
+		MissingPasswordDetails:    "You need to provide a password to create/login to an account.",
+		MissingEmailTitle:         "Missing required email",
+		MissingEmailDetails:       "You need to provide an email to create/login to an account",
+		PasswordDoNotMatchTitle:   "Passwords do not match",
+		PasswordDoNotMatchDetails: "The two passwords provided do not match.",
+		CreatedBy:                 "Powered by Nuntio.",
+	}
 	if err := cr.EncryptConfig(actionCreate, create); err != nil {
 		return nil, err
 	}

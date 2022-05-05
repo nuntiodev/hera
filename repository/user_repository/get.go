743af5118d1eaf22b9f2e7a91d1dc84eecc5d7b6
Oscar A. B. Orellana <oscar@softcorp.io>
2022-05-05 20:54:14 +0200

    updated

--- repository/user_repository/get.go
+++ repository/user_repository/get.go
@@ -2,6 +2,7 @@ package user_repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/nuntiodev/block-proto/go_block"
 )
 
@@ -16,7 +17,7 @@ func (r *mongodbRepository) Get(ctx context.Context, user *go_block.User, upgrad
 	}
 	resp := User{}
 	if err := r.collection.FindOne(ctx, filter).Decode(&resp); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not find user with id: %v and err: %v", filter, err)
 	}
 	if resp.InternalEncryptionLevel > 0 || resp.ExternalEncryptionLevel > 0 {
 		if err := r.decryptUser(&resp); err != nil {

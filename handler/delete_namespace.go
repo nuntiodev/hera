6623ca6b53610fb66c192edfeee873d8a10f080d
Oscar A. B. Orellana <oscar@softcorp.io>
2022-06-14 12:27:40 +0200

    updated

--- handler/delete_namespace.go
+++ handler/delete_namespace.go
@@ -2,10 +2,6 @@ package handler
 
 import (
 	"context"
-	"github.com/nuntiodev/nuntio-user-block/repository/config_repository"
-	"github.com/nuntiodev/nuntio-user-block/repository/user_repository"
-	"golang.org/x/sync/errgroup"
-
 	"github.com/nuntiodev/block-proto/go_block"
 )
 
@@ -13,31 +9,5 @@ import (
 	DeleteNamespace - this method deletes an entire namespace. This includes deleting all users and the namespace config.
 */
 func (h *defaultHandler) DeleteNamespace(ctx context.Context, req *go_block.UserRequest) (*go_block.UserResponse, error) {
-	var (
-		userRepo   user_repository.UserRepository
-		configRepo config_repository.ConfigRepository
-		errGroup   = &errgroup.Group{}
-		err        error
-	)
-	// async action 1 - delete all users
-	errGroup.Go(func() error {
-		userRepo, err = h.repository.UserRepositoryBuilder().SetNamespace(req.Namespace).Build(ctx)
-		if err != nil {
-			return err
-		}
-		return userRepo.DeleteAll(ctx)
-	})
-	// async action 2 - delete config
-	errGroup.Go(func() error {
-		configRepo, err = h.repository.Config(ctx, req.Namespace, req.EncryptionKey)
-		if err != nil {
-			return err
-		}
-		return configRepo.Delete(ctx)
-	})
-	if err = errGroup.Wait(); err != nil {
-		return &go_block.UserResponse{}, err
-	}
-	err = errGroup.Wait()
-	return &go_block.UserResponse{}, err
+	return &go_block.UserResponse{}, h.repository.DropDatabase(ctx, req.Namespace)
 }
--- repository/repository.go
+++ repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"github.com/nuntiodev/nuntio-user-block/repository/config_repository"
 	"github.com/nuntiodev/nuntio-user-block/repository/email_repository"
 	"github.com/nuntiodev/nuntio-user-block/repository/measurement_repository"
@@ -23,6 +24,7 @@ type Repository interface {
 	Measurements(ctx context.Context, namespace string) (measurement_repository.MeasurementRepository, error)
 	Config(ctx context.Context, namespace, externalEncryptionKey string) (config_repository.ConfigRepository, error)
 	Email(ctx context.Context, namespace, externalEncryptionKey string) (email_repository.EmailRepository, error)
+	DropDatabase(ctx context.Context, namespace string) error
 }
 
 type defaultRepository struct {
@@ -99,6 +101,13 @@ func (r *defaultRepository) Email(ctx context.Context, namespace, externalEncryp
 		return nil, err
 	}
 	return emailRepository, nil
+}
+
+func (r *defaultRepository) DropDatabase(ctx context.Context, namespace string) error {
+	if namespace == "" {
+		return errors.New("missing required namespace")
+	}
+	return r.mongodbClient.Database(namespace).Drop(ctx)
 }
 
 func New(mongoClient *mongo.Client, encryptionKeys []string, zapLog *zap.Logger, maxEmailVerificationAge time.Duration) (Repository, error) {

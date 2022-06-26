c7c4972cbef44bbe2c94f5299e37f20136b03ee2
Oscar A. B. Orellana <oscar@softcorp.io>
2022-06-26 18:05:28 +0200

    updated

---
+++ server/http_server/routes.go
@@ -0,0 +1,15 @@
+package http_server
+
+const (
+	userBasePath = "/hera/user"
+	userCreate   = "/create"
+	userGet      = "/user"
+	usersGet     = "/users"
+	userList     = "/list"
+)
+const (
+	configBasePath = "/hera/config"
+)
+const (
+	tokenBasePath = "/hera/token"
+)
